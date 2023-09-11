cbf7431d1716a764d45b756e278e013b5740ab5a
Diego <diego.zucca@correo.ucu.edu.uy>
2023-09-11 20:09:12 -0300

    Cambios

--- arbol_binario/src/main/java/com/arbol_binario/Lista.java
+++ arbol_binario/src/main/java/com/arbol_binario/Lista.java
@@ -22,7 +22,9 @@ public class Lista<T> implements ILista<T> {
     }
     public void insertarOrdenado(Nodo<T> nodo){
         // TODO: insercion ordenada
-        if(nodo == null) 
+        if(nodo == null){
+        // Falta
+        }
     }
 
     @Override
@@ -36,43 +38,96 @@ public class Lista<T> implements ILista<T> {
         }
         return null;
     }
+
     public Nodo<T> getPrimero(){
         return primero;
     }
+
     @Override
     public boolean eliminar(Comparable clave) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'eliminar'");
+        if (esVacia()) {
+            return false;
+        }
+        if (primero.getSiguiente() == null) {
+            if (primero.getEtiqueta().equals(clave)) {
+                primero = null;
+                return true;
+            }
+        }
+        Nodo<T> aux = primero;
+        if (aux.getEtiqueta().compareTo(clave) == 0) {
+            //Eliminamos el primer elemento
+            Nodo<T> temp1 = aux;
+            Nodo<T> temp = aux.getSiguiente();
+            primero = temp;
+            return true;
+        }
+        while (aux.getSiguiente() != null) {
+            if (aux.getSiguiente().getEtiqueta().equals(clave)) {
+                Nodo<T> temp = aux.getSiguiente();
+                aux.setSiguiente(temp.getSiguiente());
+                return true;
+
+            }
+            aux = aux.getSiguiente();
+        }
+        return false;
     }
 
     @Override
     public String imprimir() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'imprimir'");
+        String aux = "";
+        if (!esVacia()) {
+            Nodo<T> temp = primero;
+            while (temp != null) {
+                temp.imprimirEtiqueta();
+                temp = temp.getSiguiente();
+            }
+        }
+        return aux;
     }
 
-    @Override
     public String imprimir(String separador) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'imprimir'");
+        String aux = "";
+        if (esVacia()) {
+            return "";
+        } else {
+            Nodo<T> temp = primero;
+            aux = "" + temp.getEtiqueta();
+            while (temp.getSiguiente() != null) {
+                aux = aux + separador + temp.getSiguiente().getEtiqueta();
+                temp = temp.getSiguiente();
+            }
+
+        }
+        return aux;
+
     }
 
     @Override
     public int cantElementos() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'cantElementos'");
+        int contador = 0;
+        if (esVacia()) {
+            System.out.println("Cantidad de elementos 0.");
+            return 0;
+        } else {
+            INodo aux = primero;
+            while (aux != null) {
+                contador++;
+                aux = aux.getSiguiente();
+            }
+        }
+        return contador;
     }
 
     @Override
     public boolean esVacia() {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'esVacia'");
+        return primero == null;
     }
 
     @Override
     public void setPrimero(Nodo<T> unNodo) {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'setPrimero'");
+        this.primero = unNodo;
     }
 
     // implementar los metodos indicados en la interfaz
